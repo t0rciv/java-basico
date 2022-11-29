588f9615f81f25097a625361905f1007e6f965e7
Victor Oliveira <victor_assis@live.com>
2022-11-29 17:22:17 -0300

    study methods

--- metodos/src/SmarTv.java
+++ metodos/src/SmarTv.java
@@ -1,5 +1,21 @@
 public class SmarTv {
-    boolean on = true;
+    boolean on = false;
     int channel = 2;
     int volume = 18;
+
+    public void turnOn() {
+        on = true;
+    }
+
+    public void turnOff() {
+        on = false;
+    }
+
+    public void turnUpVolume() {
+        volume++;
+    }
+
+    public void changeChannel(int newChannel) {
+        channel = newChannel;
+    }
 }
--- metodos/src/Usuario.java
+++ metodos/src/Usuario.java
@@ -2,11 +2,19 @@ public class Usuario {
     public static void main(String[] args) throws Exception {
         SmarTv smarTv = new SmarTv();
 
+        smarTv.turnOn();
+
         String on = smarTv.on == true ? "Sim" : "Não";
 
         System.out.println("A televisão está ligada? " + on);
         System.out.println("O canal atual é: " + smarTv.channel);
         System.out.println("Está no volume: " + smarTv.volume);
+
+        smarTv.turnUpVolume();
+        System.out.println("Depois de aumentar o volume ficou em: " + smarTv.volume);
+
+        smarTv.changeChannel(5);
+        System.out.println("o canal foi alterado para o: " + smarTv.channel);
 
     }
 }
